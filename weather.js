a949066a40ef9bf4fdeb381066f5b2469a3fdb08
austin stuettgen <astuettgen@madisoncollege.edu>
2019-11-25 16:05:16 -0600

    work done on Nov 25

--- weather.js
+++ weather.js
@@ -12,8 +12,9 @@ $("#search_btn").click(function() {
    //variables for the open weather api url
     var apiLink = 'https://api.openweathermap.org/data/2.5/weather';
     var apiKey = 'fb2f1705a524eea914d7326b3d7baad4';
+    var apiUnits = '&units=imperial';
     var zipInput = $('#zipCode').val();
-    var apiURL = apiLink + '?zip=' + zipInput + '&appid=' + apiKey;
+    var apiURL = apiLink + '?zip=' + zipInput + '&appid=' + apiKey + apiUnits;
     
     console.log(apiURL);
     
@@ -26,6 +27,21 @@ $("#search_btn").click(function() {
         var temp = responseFromOW.main.temp;
         var city = responseFromOW.name;
         var description = responseFromOW.weather[0].description;
+        
+        $('.new').html(responseFromOW.name);
+        $('.cloud').html(description);
+        $('.temp').html(responseFromOW.main.temp);
+        
+        if (responseFromOW.weather[0].main === 'broken clouds'){
+            
+            $('body').css('background-image', 'url(images/broken-clouds.jpg)')
+            
+        } else if (responseFromOW.weather[0].main === 'clear sky'){
+            
+            $('body').css('background-image', 'url(images/clear-sky.jpg)') 
+        }
+        
+
     });
     
 });
